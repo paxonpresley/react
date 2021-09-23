45c1de9bf8aa1abe9ff1a3f43d1fa2e3cb6a0488
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-09-22 20:14:38 -0500

    playing around w/ different ways to return text

--- birthday-reminder/src/index.js
+++ birthday-reminder/src/index.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
+import ReactDom from "react-dom";
+
+// ! stateless function
+// function Greeting() {
+//   return <h4>Fuck you lil dickhead</h4>;
+// }
+
+// ! i wouldn't use arrow functions lmao
+// const Greeting = () => {
+//     return React.createElement('h1', {}, 'yo what' );
+// };
 
 function Greeting() {
-    
+    return <div>
+        <h1>This is like how the fuck</h1>
+    </div>
 }
+
+ReactDom.render(<Greeting />, document.getElementById("root"));

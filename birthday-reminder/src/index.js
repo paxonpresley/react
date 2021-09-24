37c2f7e9ed68f158a8216f79834752519254af86
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-09-23 22:24:04 -0500

    prop workarounds :)

--- birthday-reminder/src/index.js
+++ birthday-reminder/src/index.js
@@ -20,34 +20,45 @@ import './index.css'
 //     </div>
 // 
 
-// const firstBook = {
-//     img: 'https://m.media-amazon.com/images/I/51YAf2LzdwL.jpg',
-//     title: 'Based on a True Story: Not a Memoir',
-//     author: 'Norm Macdonald'
-// }
+const firstBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41ZN75rnLLL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Peril',
+    author: 'Bob Woodward'
+}
+const secondBook = {
+    img: 'https://m.media-amazon.com/images/I/51YAf2LzdwL.jpg',
+    title: 'Based on a True Story: Not a Memoir',
+    author: 'Norm Macdonald'
+}
 
-// my variables
-const title = 'Peril';
-const author = 'Bob Woodward';
-const img = 'https://images-na.ssl-images-amazon.com/images/I/41ZN75rnLLL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg';
+// ! // my variables
+// const title = 'Peril';
+// const author = 'Bob Woodward';
+// const img = 'https://images-na.ssl-images-amazon.com/images/I/41ZN75rnLLL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg';
 
 function BookList () {
     return (
     <section className="book">
-        <Book title="Peril" number={22} />
-        <Book author="BOBBY"/>
+        {/* how to display props */}
+        <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author} />
+        <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author} />
     </section>
     );
 }
 
-function Book (props) {
-    console.log(props);
+function Book ({img, title, author}) {
+    // console.log(props);
+    // const {img, title, author} = props;
     return <article>
-        <img src={img} alt="uh" />
+        <img src={img} alt="" />
         <h1>{title}</h1>
         <h4>{author}</h4>
-        <p>{props.title}</p>
-        <p>{props.number}</p>
     </article>;
 }
 

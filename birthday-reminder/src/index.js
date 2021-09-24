59e8955ea4098f949f5b72661f8332c3697a3726
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-09-23 22:06:43 -0500

    using props

--- birthday-reminder/src/index.js
+++ birthday-reminder/src/index.js
@@ -18,26 +18,36 @@ import './index.css'
 //     return <div>
 //         <h1>This is like how the fuck</h1>
 //     </div>
+// 
+
+// const firstBook = {
+//     img: 'https://m.media-amazon.com/images/I/51YAf2LzdwL.jpg',
+//     title: 'Based on a True Story: Not a Memoir',
+//     author: 'Norm Macdonald'
 // }
+
+// my variables
+const title = 'Peril';
+const author = 'Bob Woodward';
+const img = 'https://images-na.ssl-images-amazon.com/images/I/41ZN75rnLLL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg';
 
 function BookList () {
     return (
     <section className="book">
-        <Book />
+        <Book title="Peril" number={22} />
+        <Book author="BOBBY"/>
     </section>
     );
 }
 
-function Book () {
-    // refrence
-    const title = 'Peril';
-    const author = 'Bob Woodward';
-    img = 'https://images-na.ssl-images-amazon.com/images/I/41ZN75rnLLL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg';
-    // function
+function Book (props) {
+    console.log(props);
     return <article>
-        <img src={img}/>
+        <img src={img} alt="uh" />
         <h1>{title}</h1>
         <h4>{author}</h4>
+        <p>{props.title}</p>
+        <p>{props.number}</p>
     </article>;
 }
 

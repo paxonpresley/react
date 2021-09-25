fef6b44d8058657d0c97655ad5e8e24ae8baf92a
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-09-24 22:16:47 -0500

    id unique
    setting up an id in each array item to help with dynamic-ness

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -23,16 +23,19 @@ import './index.css'
 const books = [
 
     {
+        id: 1,
         img: 'https://images-na.ssl-images-amazon.com/images/I/41ZN75rnLLL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
         title: 'Peril',
         author: 'Bob Woodward'
     },
     {
+        id: 2,
         img: 'https://m.media-amazon.com/images/I/51YAf2LzdwL.jpg',
         title: 'Based on a True Story: Not a Memoir',
         author: 'Norm Macdonald'
     },
     {
+        id: 3,
         img: 'https://m.media-amazon.com/images/I/51zjOxWs0GS.jpg',
         title: 'Finding Martin Eden: Travels to find myself',
         author: 'Marika'
@@ -51,7 +54,7 @@ function BookList () {
             const {img, title, author } = book;
             console.log(book);
             return (
-                <Book book={book}></Book>
+                <Book key={book.id} book={book}></Book>
             )
         })}
     </section>
